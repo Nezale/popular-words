93411ef1cd657b397dfed87f52177ba0f8727246
Aleksander Kozyra <s15327@pjwstk.edu.pl>
2019-05-24 22:43:33 -0700

    Implemented findOneThousandMostPopularWords

--- src/main/java/pl/sii/PopularWords.java
+++ src/main/java/pl/sii/PopularWords.java
@@ -2,7 +2,13 @@ package pl.sii;
 
 import org.apache.commons.lang3.NotImplementedException;
 
-import java.util.Map;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+import static java.util.Map.Entry.comparingByValue;
+import static java.util.stream.Collectors.toMap;
 
 public class PopularWords {
 
@@ -12,7 +18,29 @@ public class PopularWords {
         result.entrySet().forEach(System.out::println);
     }
 
-    public Map<String, Long> findOneThousandMostPopularWords() {
-        throw new NotImplementedException("TODO implementation");
+    public Map<String, Long> findOneThousandMostPopularWords(){
+
+        Map<String,Long> popular = new HashMap<>();
+        String line;
+        Long number = 0L;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/main/resources/3esl.txt"));
+            while ((line =br.readLine()) != null){
+                if(popular.get(line)==null) {
+                    popular.put(line, 1L);
+                } else
+                    popular.replace(line,popular.get(line)+1);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return popular
+                .entrySet()
+                .stream()
+                .sorted(comparingByValue(Comparator.reverseOrder()))
+                .collect(
+                        toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2,
+                                LinkedHashMap::new));
+
     }
 }
